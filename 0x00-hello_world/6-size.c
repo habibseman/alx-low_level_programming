03a245338684c75e860010ea353d5fe22474979f
Habib Seman <habibseman032@gmail.com>
2023-02-17 00:19:06 +0300

    Add 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,10 @@
-#!/bin/bash
-#include <stdio.h>
+#in#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int a;
@@ -15,4 +19,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-}
+}	
